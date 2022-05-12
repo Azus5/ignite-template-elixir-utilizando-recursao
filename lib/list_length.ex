493a1151f37d6af4a504c2f68bc2ba51bab7cad3
Abner Ferrante <abnerchoba@hotmail.com>
2022-05-12 07:50:15 -0300

    add ListLength#call

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,10 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: calc(list, 0)
+
+  defp calc([], acc), do: acc
+
+  defp calc([_ | tl], acc) do
+    acc = acc + 1
+    calc(tl, acc)
   end
 end
